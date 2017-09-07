1b5f140443ab431b2ccdb9f65662938768f0bca0
Simon Rood <simon.rood@adyen.com>
2017-09-07 10:20:03 +0200

    Initial commit

---
+++ assets/js/setupCall.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+
+    // Call to the php file, performing the server call to checkoutshopper.adyen.com
+
+
+    var hostedFieldStyle = {
+        base: {
+            fontSize: '16px',
+            lineHeight: '16px',
+            fontSmoothing: 'antialiased',
+            fontFamily: '"Fakt", sans-serif, arial'
+        }
+    }
+
+    function initiateCheckout(jsonResponse) {
+
+        console.log(jsonResponse);
+
+        var checkout = chckt.checkout(jsonResponse, '.checkout', hostedFieldStyle);
+    }
+
+    $.ajax({
+        url: 'api/serverCall.php',
+        dataType:'json',
+        method:'POST',// jQuery > 1.9
+        type:'POST', //jQuery < 1.9
+        success:function(data){
+            initiateCheckout(data);
+        },
+
+        error : function(){
+
+            if(window.console && console.log){
+                console.log('### adyenCheckout::error:: args=', arguments);
+            }
+        }
+    });
+
+});
---
+++ config/timezone.php
@@ -0,0 +1,5 @@
+<?php
+/** Set the timezone */
+date_default_timezone_set("Europe/Amsterdam");
+
+$authentication = include('../config/authentication.php');
